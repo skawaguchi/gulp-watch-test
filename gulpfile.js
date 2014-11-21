3ae715311ec733e65ef01582bc52c7c76264fd8a
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2014-11-21 07:37:32 -0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var debug = require ( 'gulp-debug' );
+var gulp = require ( 'gulp' );
+var less = require ( 'gulp-less' );
+var watchLess = require ( 'gulp-watch-less' );
+
+gulp.task ( 'dev-watch', function () {
+  // main.less just imports child less files
+  gulp.src ( './app/styles/less/main.less' )
+    .pipe ( watchLess ( './app/styles/less/main.less' ) )
+    .pipe ( debug () )
+    .pipe ( less () )
+    .pipe ( gulp.dest ( './app/styles' ) )
+  ;
+});

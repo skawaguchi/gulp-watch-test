b6ffe8b2405f6a1a37915d1151e177c22710fa18
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2014-12-02 15:05:53 +0500

    Temporary fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ var watchLess = require ( 'gulp-watch-less' );
 gulp.task ( 'dev-watch', function () {
   // main.less just imports child less files
   gulp.src ( './app/styles/less/main.less' )
-    .pipe ( watchLess ( './app/styles/less/main.less' ) )
-    .pipe ( debug () )
+    .pipe ( watchLess ( './app/styles/less/main.less', function (files) {
+      return files.pipe ( debug () )
     .pipe ( less () )
-    .pipe ( gulp.dest ( './app/styles' ) )
+    .pipe ( gulp.dest ( './app/styles' ) );
+    } ) )
+    
   ;
 });
